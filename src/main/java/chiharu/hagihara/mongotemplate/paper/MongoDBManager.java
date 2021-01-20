c11e0c063435868cca0a0a694dabadbad2f694a3
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2021-01-20 22:15:00 +0900

    Refactor MongoDBManager

--- src/main/java/chiharu/hagihara/mongotemplate/paper/MongoDBManager.java
+++ src/main/java/chiharu/hagihara/mongotemplate/paper/MongoDBManager.java
@@ -3,7 +3,6 @@ package chiharu.hagihara.mongotemplate.paper;
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -98,22 +97,15 @@ public class MongoDBManager implements AutoCloseable {
     ////////////////////////////////
     //       UpdateOne Query
     ////////////////////////////////
-    public void queryUpdateOne(String filterKey, String filterValue, String updateKey, String updateValue) {
-        Document filterdoc = new Document();
-        Document updatedoc = new Document();
-        Document update = new Document();
-        filterdoc.append(filterKey, filterValue);
-        updatedoc.append(updateKey, updateValue);
-        update.append("$set", updatedoc);
-
-        coll.updateOne(filterdoc, update);
+    public void queryUpdateOne(String filter, String update) {
+        coll.updateOne(Document.parse(filter), Document.parse(update));
     }
 
     ////////////////////////////////
     //       DeleteOne Query
     ////////////////////////////////
-    public void queryDelete(String filterKey, String filterValue) {
-        coll.deleteOne(Filters.eq(filterKey, filterValue));
+    public void queryDelete(String filter) {
+        coll.deleteOne(Document.parse(filter));
     }
 
     ////////////////////////////////
@@ -127,8 +119,8 @@ public class MongoDBManager implements AutoCloseable {
     ////////////////////////////////
     //       Count Query
     ////////////////////////////////
-    public long queryCount() {
-        return coll.countDocuments();
+    public long queryCount(String doc) {
+        return coll.countDocuments(Document.parse(doc));
     }
 
     ////////////////////////////////
