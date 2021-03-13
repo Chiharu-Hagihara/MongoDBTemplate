4f67e3ef753087790c06253d2ea4e739ac44705b
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2021-03-13 22:51:46 +0900

    refactor: delete class

--- src/main/java/chiharu/hagihara/mongotemplate/paper/MongoTemplate.java
+++ src/main/java/chiharu/hagihara/mongotemplate/paper/MongoTemplate.java
@@ -1,15 +1,12 @@
 package chiharu.hagihara.mongotemplate.paper;
 
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MongoTemplate extends JavaPlugin implements Listener {
+public final class MongoTemplate extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        saveDefaultConfig();
-        this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
     }
 
     @Override
